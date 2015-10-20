4f8211512e598eb71f6f4e0c1784cbe4cd279ecb
Tiffany Bogus & Rick Klaras <student@epicodus.com>
2015-10-19 20:24:39 -0700

    Add other stuff.

--- js/scripts.js
+++ js/scripts.js
@@ -6,3 +6,18 @@ var leapYear = function(year) {
     }
 
 };
+
+$(document).ready(function() {
+  $("form#leap-year").submit(function(event) {
+    var year = parseInt($("input#year").val());
+    var result = leapYear(year);
+
+    $(".year").text(year);
+    if (!result) {
+      $(".not").text("not");
+    }
+
+    $("#result").show();
+    event.preventDefault();
+  });
+});
